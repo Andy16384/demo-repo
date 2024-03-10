a31865ef11c0434eddec6fee438c17af7c18ec36
root <andy16384@gmail.com>
2024-03-10 16:09:05 +0000

    get project set

---
+++ build/CMakeFiles/demo-repo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo-repo.dir/main.cc.o"
+  "CMakeFiles/demo-repo.dir/main.cc.o.d"
+  "main.o"
+  "main.o.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo-repo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
