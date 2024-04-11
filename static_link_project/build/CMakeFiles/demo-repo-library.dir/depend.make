46283ad7afd67431115696a3201d5cb64b64bab5
root <andy16384@gmail.com>
2024-04-11 13:00:18 +0000

    fix

--- static_link_project/build/CMakeFiles/demo-repo-library.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for demo-repo-library.
-# This may be replaced when dependencies are built.
--- static_link_project/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /desktop/demo-repo/static_link_project
-# Build directory: /desktop/demo-repo/static_link_project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/desktop/demo-repo/static_link_project/build/unit_tests")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/desktop/demo-repo/static_link_project/CMakeLists.txt;21;add_test;/desktop/demo-repo/static_link_project/CMakeLists.txt;0;")
